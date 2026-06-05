0150940fe79e525178242d0947cf4710a3927c6e
Intikhab Khursheed <intikhabkhursheed@gmail.com>
2026-06-05 16:33:28 +0500

    fix: handle vercel auth preflights without wildcard routes

--- server/app.js
+++ server/app.js
@@ -87,7 +87,12 @@ if (mongoURI) {
 
 // Core middleware - MUST be before routes.
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return cors(corsOptions)(req, res, next);
+  }
+  return next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
